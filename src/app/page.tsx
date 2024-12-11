b4bc82dc18759a4830ee4dd44973e0861cbea0e7
pratul03 <kk953504@gmail.com>
2024-12-12 00:30:50 +0530

    n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import CodeEditor from "@/components/editor";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <div className="flex w-screen flex-col h-screen bg-background">
-      <div className="h-12 flex">Navbar</div>
+      <div className="h-12 flex">
+        <Navbar />
+      </div>
       <div className="w-screen flex flex-grow">
         <CodeEditor />
       </div>
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../ui/button";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <Button variant={"secondary"} size={"sm"}>
+          Navbar
+        </Button>
+      </div>
+    </div>
+  );
+}
