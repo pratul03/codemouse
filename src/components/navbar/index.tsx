0482b69049c6aca500f91247d3c65ccd826f341a
pratul03 <kk953504@gmail.com>
2024-12-12 00:46:45 +0530

    n

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,11 +1,12 @@
+import { ChevronLeftIcon } from "lucide-react";
 import { Button } from "../ui/button";
 
 export default function Navbar() {
   return (
-    <div>
-      <div>
-        <Button variant={"secondary"} size={"sm"}>
-          Navbar
+    <div className="h-12 px-2 flex items-center justify-between">
+      <div className="flex items-center">
+        <Button variant={"outline"} size={"sm"}>
+          <ChevronLeftIcon className="w-4 h-4" /> Go Home
         </Button>
       </div>
     </div>
